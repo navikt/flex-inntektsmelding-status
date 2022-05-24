5062ee4b6323ba10b45144f9116ecf7ef685d56f
kulsveen <bjorn.inge.kulsveen@nav.no>
2022-05-24 11:53:19 +0200

    Spring app

---
+++ build.gradle.kts
@@ -0,0 +1,71 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "2.7.0"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21"
+}
+
+group = "no.nav.helse.flex"
+version = "1.0.0"
+description = "flex-inntektsmelding-status"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+ext["okhttp3.version"] = "4.9.0" // For at token support testen kjører (tror jeg)
+
+val githubUser: String by project
+val githubPassword: String by project
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://maven.pkg.github.com/navikt/maven-release")
+    }
+}
+
+val testContainersVersion = "1.17.2"
+val logstashLogbackEncoderVersion = "7.2"
+val kluentVersion = "1.68"
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.springframework.kafka:spring-kafka")
+    implementation("org.springframework.boot:spring-boot-starter-logging")
+    implementation("net.logstash.logback:logstash-logback-encoder:$logstashLogbackEncoderVersion")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+    implementation("org.aspectj:aspectjrt")
+    implementation("org.aspectj:aspectjweaver")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-registry-prometheus")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.testcontainers:kafka:$testContainersVersion")
+    testImplementation("org.testcontainers:junit-jupiter:$testContainersVersion")
+    testImplementation("org.awaitility:awaitility")
+    testImplementation("org.amshove.kluent:kluent:$kluentVersion")
+}
+
+tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+    this.archiveFileName.set("app.jar")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+        if (System.getenv("CI") == "true") {
+            kotlinOptions.allWarningsAsErrors = true
+        }
+    }
+}
+tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging {
+        events("STANDARD_OUT", "STARTED", "PASSED", "FAILED", "SKIPPED")
+    }
+}
