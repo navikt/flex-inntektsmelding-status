e02f723ff1f501b52202d8aa558a3b33d6c1978d
kulsveen <bjorn.inge.kulsveen@nav.no>
2022-06-01 09:08:46 +0200

    MeldingKafkaDto med variant og lukkbar

--- src/main/kotlin/no/nav/helse/flex/melding/MeldingKafkaDto.kt
+++ src/main/kotlin/no/nav/helse/flex/melding/MeldingKafkaDto.kt
@@ -7,14 +7,22 @@ data class MeldingKafkaDto(
     val lukkMelding: LukkMelding?,
     val fnr: String,
 )
-
 data class LukkMelding(
     val timestamp: Instant,
 )
 
+enum class Variant {
+    info,
+    warning,
+    success,
+    error,
+}
+
 data class OpprettMelding(
     val tekst: String,
     val lenke: String,
+    val variant: Variant,
+    val lukkbar: Boolean,
     val meldingType: String,
     val synligFremTil: Instant?,
 )
