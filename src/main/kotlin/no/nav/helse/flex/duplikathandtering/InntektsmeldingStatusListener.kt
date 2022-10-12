d2caaf4338fff494f7c90ace47ec1727abd051a8
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-10-12 15:54:15 +0200

    Skaff offset

--- src/main/kotlin/no/nav/helse/flex/duplikathandtering/InntektsmeldingStatusListener.kt
+++ src/main/kotlin/no/nav/helse/flex/duplikathandtering/InntektsmeldingStatusListener.kt
@@ -23,7 +23,8 @@ class InntektsmeldingStatusListener(
         containerFactory = "aivenKafkaListenerContainerFactory",
         id = "flex-inntektsmelding-status-inntektsmelding-duplikathandtering",
         idIsGroup = false,
-        groupId = "duplikathandtering-dryrun"
+        groupId = "duplikathandtering-dryrun",
+        properties = ["auto.offset.reset = latest"]
     )
     fun listen(cr: ConsumerRecord<String, String>, acknowledgment: Acknowledgment) {
         val kafkaDto: InntektsmeldingKafkaDto = objectMapper.readValue(cr.value())
