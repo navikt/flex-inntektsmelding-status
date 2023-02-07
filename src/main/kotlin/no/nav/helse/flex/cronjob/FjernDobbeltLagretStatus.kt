6d828b4dd7f179f3afbc0da20155c94cbad9fd10
kulsveen <bjorn.inge.kulsveen@nav.no>
2023-02-07 16:23:35 +0100

    Revert "Legger tilbake mangler inntektsmelding status i starten av historikk (#86)"
    This reverts commit 3c8a5fd825e06fa32e32517664613f9307284300.

--- src/main/kotlin/no/nav/helse/flex/cronjob/FjernDobbeltLagretStatus.kt
+++ src/main/kotlin/no/nav/helse/flex/cronjob/FjernDobbeltLagretStatus.kt
@@ -1,18 +1,14 @@
 package no.nav.helse.flex.cronjob
 
-import no.nav.helse.flex.inntektsmelding.InntektsmeldingStatusDbRecord
-import no.nav.helse.flex.inntektsmelding.InntektsmeldingStatusRepository
 import no.nav.helse.flex.inntektsmelding.StatusRepository
 import no.nav.helse.flex.inntektsmelding.StatusVerdi
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
-import java.time.Duration
 import java.util.concurrent.TimeUnit
 
 @Component
 class FjernDobbeltLagretStatus(
-    private val statusRepository: StatusRepository,
-    private val inntektsmeldingStatusRepository: InntektsmeldingStatusRepository
+    private val statusRepository: StatusRepository
 ) {
 
     @Scheduled(initialDelay = 2, fixedDelay = 120, timeUnit = TimeUnit.MINUTES)
@@ -30,27 +26,13 @@ class FjernDobbeltLagretStatus(
         sisteStatusManglerInntektsmelding.forEach { inntektsmeldingMedStatusHistorikk ->
             val statusVerdier = inntektsmeldingMedStatusHistorikk.statusHistorikk.map { it.status }
             if (statusVerdier == listOf(
+                    StatusVerdi.MANGLER_INNTEKTSMELDING,
                     StatusVerdi.BRUKERNOTIFIKSJON_MANGLER_INNTEKTSMELDING_SENDT,
                     StatusVerdi.DITT_SYKEFRAVAER_MANGLER_INNTEKTSMELDING_SENDT,
                     StatusVerdi.MANGLER_INNTEKTSMELDING
                 )
             ) {
-                val brukernot = inntektsmeldingMedStatusHistorikk.statusHistorikk.first()
-                val brukernotStatus = inntektsmeldingStatusRepository.findById(brukernot.id).get()
-                val mangler = inntektsmeldingMedStatusHistorikk.statusHistorikk.last()
-                val manglerStaus = inntektsmeldingStatusRepository.findById(mangler.id).get()
-
-                require(brukernotStatus.opprettet.isBefore(manglerStaus.opprettet))
-
-                inntektsmeldingStatusRepository.save(
-                    InntektsmeldingStatusDbRecord(
-                        inntektsmeldingId = inntektsmeldingMedStatusHistorikk.id,
-                        opprettet = brukernotStatus.opprettet.minusSeconds(Duration.ofDays(29).toSeconds()),
-                        status = StatusVerdi.MANGLER_INNTEKTSMELDING
-                    )
-                )
-
-                statusRepository.slettduplikatStatus(mangler.id)
+                statusRepository.slettduplikatStatus(inntektsmeldingMedStatusHistorikk.statusHistorikk.first().id)
             }
         }
     }
--- src/test/kotlin/no/nav/helse/flex/cronjob/FjernDobbeltLagretStatusTest.kt
+++ src/test/kotlin/no/nav/helse/flex/cronjob/FjernDobbeltLagretStatusTest.kt
@@ -29,6 +29,7 @@ class FjernDobbeltLagretStatusTest : FellesTestOppsett() {
     fun `sletter alle duplikat manglerinntektsmeldingstatus`() {
         val inntektsmelding = inntektsmelding.copy(
             statusHistorikk = listOf(
+                StatusHistorikk("id", StatusVerdi.MANGLER_INNTEKTSMELDING),
                 StatusHistorikk("id", StatusVerdi.BRUKERNOTIFIKSJON_MANGLER_INNTEKTSMELDING_SENDT),
                 StatusHistorikk("id", StatusVerdi.DITT_SYKEFRAVAER_MANGLER_INNTEKTSMELDING_SENDT),
                 StatusHistorikk("id", StatusVerdi.MANGLER_INNTEKTSMELDING),
@@ -63,9 +64,9 @@ class FjernDobbeltLagretStatusTest : FellesTestOppsett() {
         statusRepository.hentInntektsmeldingMedStatusHistorikk(id)!!.statusHistorikk.map {
             it.status
         } `should be equal to` listOf(
-            StatusVerdi.MANGLER_INNTEKTSMELDING,
             StatusVerdi.BRUKERNOTIFIKSJON_MANGLER_INNTEKTSMELDING_SENDT,
             StatusVerdi.DITT_SYKEFRAVAER_MANGLER_INNTEKTSMELDING_SENDT,
+            StatusVerdi.MANGLER_INNTEKTSMELDING
         )
     }
 }
