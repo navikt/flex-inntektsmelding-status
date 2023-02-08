f60f31c0cd31e26ef8fa5641d70c1abb9a553c91
golshidbahadorian <golshid.bahadorian@nav.no>
2023-02-08 09:34:31 +0100

    Revert "Ferdig med opprydding (#88)"
    This reverts commit c74313dcd1080696b12d7af66f5c4c883a5d15aa.

---
+++ src/main/kotlin/no/nav/helse/flex/cronjob/FjernDobbeltLagretStatus.kt
@@ -0,0 +1,48 @@
+package no.nav.helse.flex.cronjob
+
+import no.nav.helse.flex.inntektsmelding.InntektsmeldingStatusRepository
+import no.nav.helse.flex.inntektsmelding.StatusRepository
+import no.nav.helse.flex.inntektsmelding.StatusVerdi
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import java.util.concurrent.TimeUnit
+
+@Component
+class FjernDobbeltLagretStatus(
+    private val statusRepository: StatusRepository,
+    private val inntektsmeldingStatusRepository: InntektsmeldingStatusRepository
+) {
+
+    @Scheduled(initialDelay = 1, fixedDelay = 5, timeUnit = TimeUnit.MINUTES)
+    fun job() {
+        fjernDuplikatInntektsmeldingstatus()
+    }
+
+    fun fjernDuplikatInntektsmeldingstatus() {
+
+        val sisteStatusManglerInntektsmelding = statusRepository
+            .hentAlleMedNyesteStatus(StatusVerdi.MANGLER_INNTEKTSMELDING)
+            .map { statusRepository.hentInntektsmeldingMedStatusHistorikk(it.id)!! }
+
+        sisteStatusManglerInntektsmelding.forEach { inntektsmeldingMedStatusHistorikk ->
+            val statusVerdier = inntektsmeldingMedStatusHistorikk.statusHistorikk.map { it.status }
+            if (statusVerdier == listOf(
+                    StatusVerdi.TRENGER_IKKE_INNTEKTSMELDING,
+                    StatusVerdi.MANGLER_INNTEKTSMELDING,
+                    StatusVerdi.BRUKERNOTIFIKSJON_MANGLER_INNTEKTSMELDING_SENDT,
+                    StatusVerdi.DITT_SYKEFRAVAER_MANGLER_INNTEKTSMELDING_SENDT,
+                    StatusVerdi.MANGLER_INNTEKTSMELDING,
+                )
+            ) {
+                val mangler = inntektsmeldingMedStatusHistorikk.statusHistorikk.last()
+                val manglerStaus = inntektsmeldingStatusRepository.findById(mangler.id).get()
+                val nestSiste = inntektsmeldingMedStatusHistorikk.statusHistorikk.last { it.id != mangler.id }
+                val nestSisteStatus = inntektsmeldingStatusRepository.findById(nestSiste.id).get()
+
+                require(nestSisteStatus.opprettet.isBefore(manglerStaus.opprettet))
+
+                statusRepository.slettduplikatStatus(mangler.id)
+            }
+        }
+    }
+}
