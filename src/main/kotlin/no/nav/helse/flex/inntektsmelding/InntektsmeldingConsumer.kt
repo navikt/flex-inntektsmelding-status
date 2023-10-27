8885b82b3750564ad2ce5baf23c3fcc71d0d8227
kulsveen <bjorn.inge.kulsveen@nav.no>
2023-10-27 12:55:40 +0200

    Skipper orgnummer som ikke lengre finnes i dev

--- src/main/kotlin/no/nav/helse/flex/inntektsmelding/InntektsmeldingConsumer.kt
+++ src/main/kotlin/no/nav/helse/flex/inntektsmelding/InntektsmeldingConsumer.kt
@@ -2,7 +2,9 @@ package no.nav.helse.flex.inntektsmelding
 
 import com.fasterxml.jackson.module.kotlin.readValue
 import no.nav.helse.flex.kafka.inntektsmeldingstatusTopic
+import no.nav.helse.flex.logger
 import no.nav.helse.flex.objectMapper
+import no.nav.helse.flex.util.EnvironmentToggles
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.support.Acknowledgment
@@ -10,8 +12,11 @@ import org.springframework.stereotype.Component
 
 @Component
 class InntektsmeldingConsumer(
-    private val inntektsmeldingService: InntektsmeldingService
+    private val inntektsmeldingService: InntektsmeldingService,
+    private val environmentToggles: EnvironmentToggles,
 ) {
+    private val log = logger()
+
     @KafkaListener(
         topics = [inntektsmeldingstatusTopic],
         containerFactory = "aivenKafkaListenerContainerFactory",
@@ -21,7 +26,15 @@ class InntektsmeldingConsumer(
     fun listen(cr: ConsumerRecord<String, String>, acknowledgment: Acknowledgment) {
         val kafkaDto: InntektsmeldingKafkaDto = objectMapper.readValue(cr.value())
 
-        inntektsmeldingService.prosesserKafkaMelding(kafkaDto)
+        try {
+            inntektsmeldingService.prosesserKafkaMelding(kafkaDto)
+        } catch (e: RuntimeException) {
+            if (!environmentToggles.isProduction() && e.message?.contains("Finner ikke orgnummer") == true) {
+                log.info("Finner ikke orgnummer i dev, går videre", e)
+            } else {
+                throw e
+            }
+        }
 
         acknowledgment.acknowledge()
     }
