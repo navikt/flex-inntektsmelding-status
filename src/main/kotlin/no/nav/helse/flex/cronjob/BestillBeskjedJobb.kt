656a2b99c7c1ba8fee116cb1eedf92eb7b8715ef
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-03-14 14:32:49 +0100

    Logger ned overlapp til db

--- src/main/kotlin/no/nav/helse/flex/cronjob/BestillBeskjedJobb.kt
+++ src/main/kotlin/no/nav/helse/flex/cronjob/BestillBeskjedJobb.kt
@@ -6,8 +6,10 @@ import no.nav.helse.flex.logger
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import java.time.DayOfWeek
 import java.time.Instant
 import java.time.OffsetDateTime
+import java.util.Calendar.SATURDAY
 import java.util.concurrent.TimeUnit
 
 @Component
@@ -20,29 +22,43 @@ class BestillBeskjedJobb(
 
     private fun sykmeldtVarsel() = OffsetDateTime.now().minusDays(ventetid).toInstant()
 
-    @Scheduled(initialDelay = 2, fixedDelay = 30, timeUnit = TimeUnit.MINUTES)
+    @Scheduled(initialDelay = 1, fixedDelay = 1, timeUnit = TimeUnit.MINUTES)
     fun job() {
+        // Returner direkte hvis det er kveldstid eller helg
+        if (OffsetDateTime.now().dayOfWeek in setOf(DayOfWeek.SUNDAY, DayOfWeek.SATURDAY)) {
+            log.info("Det er helg, jobben kjøres ikke")
+            return
+        }
+        if (OffsetDateTime.now().hour < 9 || OffsetDateTime.now().hour > 15) {
+            log.info("Det er ikke dagtid, jobben kjøres ikke")
+            return
+        }
+        log.info("Kjører jobb for å bestille beskjeder for inntektsmeldinger som mangler etter $ventetid dager")
         jobMedParameter(opprettetFor = sykmeldtVarsel())
     }
 
     fun jobMedParameter(opprettetFor: Instant) {
         var beskjederBestilt = 0
 
-        val manglerBeskjed =
+        val manglerImEldreEnnXdager =
             statusRepository
                 .hentAlleMedNyesteStatus(StatusVerdi.MANGLER_INNTEKTSMELDING)
                 .filter { it.statusOpprettet.isBefore(opprettetFor) }
                 .sortedByDescending { it.vedtakFom }
-                .take(200)
 
-        manglerBeskjed.forEach {
-            if (bestillBeskjed.opprettVarsler(it)) {
-                beskjederBestilt++
+        log.info("Fant ${manglerImEldreEnnXdager.size} inntektsmeldinger som mangler etter $ventetid dager")
+
+        manglerImEldreEnnXdager
+            .take(200).forEach {
+                if (bestillBeskjed.opprettVarsler(it)) {
+                    beskjederBestilt++
+                }
             }
-        }
 
         if (beskjederBestilt > 0) {
-            log.info("Behandlet $beskjederBestilt antall inntektsmeldinger som mangler etter 4 uker")
+            log.info("Behandlet $beskjederBestilt antall inntektsmeldinger som mangler etter $ventetid dager")
+        } else {
+            log.info("Ingen inntektsmeldinger som mangler etter $ventetid dager ble behandlet")
         }
     }
 }
