0b6ae1c92fd858c690d7512aef99863ce83dbd20
Oivind Thorsen Kulsrud <oivind.thorsen.kulsrud@nav.no>
2024-04-11 08:52:02 +0200

    wip

--- src/main/kotlin/no/nav/helse/flex/vedtak/VedtakRepository.kt
+++ src/main/kotlin/no/nav/helse/flex/vedtak/VedtakRepository.kt
@@ -1,24 +1,18 @@
-import com.fasterxml.jackson.module.kotlin.readValue
-import no.nav.helse.flex.objectMapper
-import java.time.LocalDate
-import java.util.*
-
-
-
 import org.springframework.data.annotation.Id
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
+import java.time.LocalDate
 import java.time.Instant
 
 @Repository
 interface VedtakRepository : CrudRepository<Vedtak, String> {
-    fun findByVedtakUuid(VedtakUuid: String): Vedtak?
+    fun findByVedtakUuid(vedtakUuid: String): Vedtak?
 }
 
 data class Vedtak(
     @Id
     val id: String? = null,
-    val VedtakUuid: String,
+    val vedtakUuid: String,
     val orgnummer: String?,
     val soknadstype: String,
     val fom: LocalDate,
