47117e228f23de896357036be53c8a911ef265b3
Oivind Thorsen Kulsrud <oivind.thorsen.kulsrud@nav.no>
2024-04-11 08:51:42 +0200

    wip

--- src/main/kotlin/no/nav/helse/flex/vedtak/VedtakLagring.kt
+++ src/main/kotlin/no/nav/helse/flex/vedtak/VedtakLagring.kt
@@ -1,14 +1,22 @@
 package no.nav.helse.flex.sykepengesoknad
 
+import VedtakRepository
 import no.nav.helse.flex.logger
 import no.nav.helse.flex.sykepengesoknad.kafka.SoknadsstatusDTO
 import no.nav.helse.flex.sykepengesoknad.kafka.SykepengesoknadDTO
+import no.nav.helse.flex.vedtak.VedtakDto
 import org.springframework.stereotype.Component
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneId
-
+// wtf wtf wtf
 @Component
-class VedtakLagring {
+class VedtakLagring (
+    private val sykepengesoknadRepository: VedtakRepository
+){
+    val log = logger()
+    fun handterVedtak (vedtak: VedtakDto) {
+
+    }
 
 }
