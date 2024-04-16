1495b3c4792831411ff6e244c690d38cc56aa169
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2024-04-16 15:43:19 +0200

    Lagre inntektsmeldinger (#242)
    * Lagre inntektsmeldinger
    
    * Lagre inntektsmeldinger

---
+++ src/main/kotlin/no/nav/helse/flex/inntektsmelding/InntektsmeldingDbRecord.kt
@@ -0,0 +1,20 @@
+package no.nav.helse.flex.inntektsmelding
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
+import java.time.Instant
+import java.time.LocalDate
+
+@Table("inntektsmelding")
+data class InntektsmeldingDbRecord(
+    @Id
+    val id: String? = null,
+    val inntektsmeldingId: String,
+    val fnr: String,
+    val arbeidsgivertype: String,
+    val virksomhetsnummer: String?,
+    val fullRefusjon: Boolean,
+    val opprettet: Instant,
+    val mottattDato: Instant,
+    val foersteFravaersdag: LocalDate?,
+)
---
+++ src/main/kotlin/no/nav/helse/flex/inntektsmelding/LagreInntektsmeldingerFraKafka.kt
@@ -0,0 +1,42 @@
+package no.nav.helse.flex.inntektsmelding
+
+import com.fasterxml.jackson.module.kotlin.readValue
+import no.nav.helse.flex.logger
+import no.nav.helse.flex.objectMapper
+import no.nav.inntektsmeldingkontrakt.Inntektsmelding
+import org.springframework.stereotype.Component
+import java.time.Instant
+import java.time.ZoneId
+
+@Component
+class LagreInntektsmeldingerFraKafka(
+    val inntektsmeldingRepository: InntektsmeldingRepository,
+) {
+    val log = logger()
+
+    fun oppdater(value: String) {
+        val inntektsmelding: Inntektsmelding = objectMapper.readValue(value)
+        if (inntektsmeldingRepository.existsByInntektsmeldingId(inntektsmelding.inntektsmeldingId)) {
+            log.info("Inntektsmelding med id ${inntektsmelding.inntektsmeldingId} finnes allerede i databasen")
+            return
+        }
+
+        val beregnetInntektPerMnd = inntektsmelding.beregnetInntekt?.toInt() ?: 0
+        val refusjonPerMnd = inntektsmelding.refusjon.beloepPrMnd?.toInt() ?: 0
+        val fullRefusjon = beregnetInntektPerMnd == refusjonPerMnd
+
+        inntektsmeldingRepository.save(
+            InntektsmeldingDbRecord(
+                inntektsmeldingId = inntektsmelding.inntektsmeldingId,
+                mottattDato = inntektsmelding.mottattDato.atZone(ZoneId.of("Europe/Oslo")).toInstant(),
+                opprettet = Instant.now(),
+                fnr = inntektsmelding.arbeidstakerFnr,
+                arbeidsgivertype = inntektsmelding.arbeidsgivertype.toString(),
+                fullRefusjon = fullRefusjon,
+                virksomhetsnummer = inntektsmelding.virksomhetsnummer,
+                foersteFravaersdag = inntektsmelding.foersteFravaersdag,
+            ),
+        )
+        log.info("Lagret inntektsmelding med id ${inntektsmelding.inntektsmeldingId} i databasen")
+    }
+}
