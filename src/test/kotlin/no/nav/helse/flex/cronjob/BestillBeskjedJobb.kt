3f9ddf7244ca7756dc8ea2dfc2a20bc577d4e127
Oivind Thorsen Kulsrud <oivind.thorsen.kulsrud@nav.no>
2024-05-08 10:56:03 +0200

    Fjerner scheduled utsending av varsler om manglende IM

--- src/test/kotlin/no/nav/helse/flex/cronjob/BestillBeskjedJobb.kt
+++ src/test/kotlin/no/nav/helse/flex/cronjob/BestillBeskjedJobb.kt
@@ -4,12 +4,9 @@ import no.nav.helse.flex.logger
 import no.nav.helse.flex.vedtaksperiode.StatusRepository
 import no.nav.helse.flex.vedtaksperiode.StatusVerdi
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
-import java.time.DayOfWeek
 import java.time.Instant
 import java.time.OffsetDateTime
-import java.util.concurrent.TimeUnit
 
 @Component
 class BestillBeskjedJobb(
@@ -20,21 +17,6 @@ class BestillBeskjedJobb(
     private val log = logger()
 
     private fun sykmeldtVarsel() = OffsetDateTime.now().minusDays(ventetid).toInstant()
-
-    @Scheduled(initialDelay = 2, fixedDelay = 30, timeUnit = TimeUnit.MINUTES)
-    fun job() {
-        // Returner direkte hvis det er kveldstid eller helg
-        if (OffsetDateTime.now().dayOfWeek in setOf(DayOfWeek.SUNDAY, DayOfWeek.SATURDAY)) {
-            log.info("Det er helg, jobben kjøres ikke")
-            return
-        }
-        if (OffsetDateTime.now().hour < 9 || OffsetDateTime.now().hour > 15) {
-            log.info("Det er ikke dagtid, jobben kjøres ikke")
-            return
-        }
-        log.info("Kjører jobb for å bestille beskjeder for inntektsmeldinger som mangler etter $ventetid dager")
-        jobMedParameter(opprettetFor = sykmeldtVarsel())
-    }
 
     fun jobMedParameter(opprettetFor: Instant) {
         var beskjederBestilt = 0
