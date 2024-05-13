2603bc6c63c4b7a2fb346f04dd6937849c7834e3
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2024-05-13 14:27:15 +0200

    Fjerner ubrukt feilhåndtering (#265)

--- src/main/kotlin/no/nav/helse/flex/vedtaksperiodebehandling/VedtaksperiodeBehandlingConsumer.kt
+++ src/main/kotlin/no/nav/helse/flex/vedtaksperiodebehandling/VedtaksperiodeBehandlingConsumer.kt
@@ -2,9 +2,7 @@ package no.nav.helse.flex.vedtaksperiodebehandling
 
 import com.fasterxml.jackson.module.kotlin.readValue
 import no.nav.helse.flex.kafka.SIS_TOPIC
-import no.nav.helse.flex.logger
 import no.nav.helse.flex.objectMapper
-import no.nav.helse.flex.util.EnvironmentToggles
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.support.Acknowledgment
@@ -13,10 +11,7 @@ import org.springframework.stereotype.Component
 @Component
 class VedtaksperiodeBehandlingConsumer(
     private val prosseserKafkaMeldingFraSpleiselaget: ProsseserKafkaMeldingFraSpleiselaget,
-    private val environmentToggles: EnvironmentToggles,
 ) {
-    private val log = logger()
-
     @KafkaListener(
         topics = [SIS_TOPIC],
         containerFactory = "aivenKafkaListenerContainerFactory",
@@ -29,16 +24,7 @@ class VedtaksperiodeBehandlingConsumer(
     ) {
         val kafkaDto: Behandlingstatusmelding = objectMapper.readValue(cr.value())
 
-        try {
-            prosseserKafkaMeldingFraSpleiselaget.prosesserKafkaMelding(kafkaDto)
-        } catch (e: RuntimeException) {
-            if (!environmentToggles.isProduction() && e.message?.contains("Finner ikke orgnummer") == true) {
-                log.info("Finner ikke orgnummer i dev, går videre", e)
-            } else {
-                throw e
-            }
-        }
-
+        prosseserKafkaMeldingFraSpleiselaget.prosesserKafkaMelding(kafkaDto)
         acknowledgment.acknowledge()
     }
 }
