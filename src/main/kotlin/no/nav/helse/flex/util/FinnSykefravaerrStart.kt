838262f7d6b8a6eb3fdc58af73d9191cbec6f46d
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2024-05-21 13:44:16 +0200

    Fjerner kode som leser data fra topic som skal pensjoneres
    Nytt topic er tbd.sis

--- src/main/kotlin/no/nav/helse/flex/util/FinnSykefravaerrStart.kt
+++
@@ -1,9 +0,0 @@
-package no.nav.helse.flex.util
-
-import no.nav.helse.flex.vedtaksperiode.VedtaksperiodeMedStatus
-import java.time.LocalDate
-
-fun List<VedtaksperiodeMedStatus>.finnSykefraværStart(fom: LocalDate): LocalDate {
-    this.find { it.vedtakTom.erRettFør(fom) }?.let { return finnSykefraværStart(it.vedtakFom) }
-    return fom
-}
