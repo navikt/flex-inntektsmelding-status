6aa6a3645dce372162626de2448abdeab8ecb3a3
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-06-04 12:38:26 +0200

    Søker partionions og konsumerer

--- src/main/kotlin/no/nav/helse/flex/vedtaksperiodebehandling/VedtaksperiodeBehandlingConsumer.kt
+++ src/main/kotlin/no/nav/helse/flex/vedtaksperiodebehandling/VedtaksperiodeBehandlingConsumer.kt
@@ -2,18 +2,23 @@ package no.nav.helse.flex.vedtaksperiodebehandling
 
 import com.fasterxml.jackson.module.kotlin.readValue
 import no.nav.helse.flex.kafka.SIS_TOPIC
+import no.nav.helse.flex.logger
 import no.nav.helse.flex.objectMapper
 import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.springframework.context.annotation.Profile
+import org.apache.kafka.common.TopicPartition
 import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.listener.ConsumerSeekAware
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
+import java.time.Duration
+import java.time.Instant
 
 @Component
-@Profile("siskonsumer")
 class VedtaksperiodeBehandlingConsumer(
     private val prosseserKafkaMeldingFraSpleiselaget: ProsseserKafkaMeldingFraSpleiselaget,
-) {
+) : ConsumerSeekAware {
+    val log = logger()
+
     @KafkaListener(
         topics = [SIS_TOPIC],
         containerFactory = "aivenKafkaListenerContainerFactory",
@@ -32,5 +37,20 @@ class VedtaksperiodeBehandlingConsumer(
         }
 
         acknowledgment.acknowledge()
+    }
+
+    override fun onPartitionsAssigned(
+        assignments: Map<TopicPartition, Long>,
+        callback: ConsumerSeekAware.ConsumerSeekCallback,
+    ) {
+        val timestampToSearch = Instant.now().minus(Duration.ofDays(21)).toEpochMilli()
+        val offsetsForTimes = assignments.keys.associateWith { timestampToSearch }
+
+        log.info("Seeking to timestamp $timestampToSearch for partitions $assignments")
+        // Seek to the offsets for 3 weeks ago for each partition
+        offsetsForTimes.forEach { (partition, timestamp) ->
+            log.info("Seeking to timestamp $timestamp for partition $partition")
+            callback.seekToTimestamp(partition.topic(), partition.partition(), timestamp)
+        }
     }
 }
