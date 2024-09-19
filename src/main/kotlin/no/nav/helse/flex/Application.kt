f93009a8fd8ef58ea8d1ad85687347752ea4d489
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2024-09-19 09:16:03 +0200

    Print on Bean init

--- src/main/kotlin/no/nav/helse/flex/Application.kt
+++ src/main/kotlin/no/nav/helse/flex/Application.kt
@@ -4,6 +4,9 @@ import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.stereotype.Component
+import java.lang.management.GarbageCollectorMXBean
+import java.lang.management.ManagementFactory
 
 @SpringBootApplication
 @EnableScheduling
@@ -12,4 +15,18 @@ class Application
 
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
+}
+
+@Component
+class EnvironmentDebug {
+    init {
+
+        logger().info("EnvironmentDebug: JDK_JAVA_OPTIONS: {}", System.getenv("JDK_JAVA_OPTIONS"))
+
+        val gcBeans = ManagementFactory.getGarbageCollectorMXBeans()
+
+        gcBeans.forEach({ gcBean: GarbageCollectorMXBean ->
+            logger().info("EnvironmentDebug:: GarbageCollectorMXBean: {}", gcBean.name)
+        })
+    }
 }
