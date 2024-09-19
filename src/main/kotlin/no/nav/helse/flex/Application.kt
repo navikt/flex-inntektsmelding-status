52e3e2402344fef53b70baccde12f4ef6bb4d224
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2024-09-19 09:59:35 +0200

    Print gcFlags

--- src/main/kotlin/no/nav/helse/flex/Application.kt
+++ src/main/kotlin/no/nav/helse/flex/Application.kt
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.stereotype.Component
-import java.lang.management.GarbageCollectorMXBean
 import java.lang.management.ManagementFactory
+import javax.management.ObjectName
 
 @SpringBootApplication
 @EnableScheduling
@@ -21,12 +21,27 @@ fun main(args: Array<String>) {
 class EnvironmentDebug {
     init {
 
-        logger().info("EnvironmentDebug: JDK_JAVA_OPTIONS: {}", System.getenv("JDK_JAVA_OPTIONS"))
+        val javaOptions = System.getenv("JDK_JAVA_OPTIONS")
+        val availableProcessors = Runtime.getRuntime().availableProcessors()
 
-        val gcBeans = ManagementFactory.getGarbageCollectorMXBeans()
+        val flags =
+            ManagementFactory.getPlatformMBeanServer().invoke(
+                ObjectName.getInstance("com.sun.management:type=DiagnosticCommand"),
+                "vmFlags",
+                arrayOf(null),
+                arrayOf("[Ljava.lang.String;"),
+            ) as String
 
-        gcBeans.forEach({ gcBean: GarbageCollectorMXBean ->
-            logger().info("EnvironmentDebug:: GarbageCollectorMXBean: {}", gcBean.name)
-        })
+        val gcFlags =
+            flags.split("\\s+".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray().find { it.contains("GC") }
+
+        val gcPairs =
+            ManagementFactory.getGarbageCollectorMXBeans()
+                .map { b -> Pair(b.name, b.memoryPoolNames.contentToString()) }
+
+        logger().info("EnvironmentDebug: JDK_JAVA_OPTIONS: $javaOptions")
+        logger().info("EnvironmentDebug: availableProcessors: $availableProcessors")
+        logger().info("EnvironmentDebug: GC: $gcFlags")
+        logger().info("EnvironmentDebug: GC beans: $gcPairs")
     }
 }
