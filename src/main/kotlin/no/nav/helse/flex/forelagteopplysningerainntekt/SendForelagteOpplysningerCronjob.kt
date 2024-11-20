3061b7a3b2d37f977114c10e35f1d6225cb01196
Eirik Skjærseth <eirik.skjaerseth@gmail.com>
2024-11-20 09:30:49 +0100

    increase cron job initial delay so they dont interfer with tests

--- src/main/kotlin/no/nav/helse/flex/forelagteopplysningerainntekt/SendForelagteOpplysningerCronjob.kt
+++ src/main/kotlin/no/nav/helse/flex/forelagteopplysningerainntekt/SendForelagteOpplysningerCronjob.kt
@@ -101,7 +101,7 @@ class SendForelagteOpplysningerCronjob(
         return CronJobStatus.SENDT_FORELAGTE_OPPLYSNINGER
     }
 
-    @Scheduled(initialDelay = 1, fixedDelay = 15, timeUnit = TimeUnit.MINUTES)
+    @Scheduled(initialDelay = 10, fixedDelay = 15, timeUnit = TimeUnit.MINUTES)
     fun run(): Map<CronJobStatus, Int> {
         val osloDatetimeNow = OffsetDateTime.now().tilOsloZone()
         if (osloDatetimeNow.dayOfWeek in setOf(DayOfWeek.SUNDAY, DayOfWeek.SATURDAY)) {
