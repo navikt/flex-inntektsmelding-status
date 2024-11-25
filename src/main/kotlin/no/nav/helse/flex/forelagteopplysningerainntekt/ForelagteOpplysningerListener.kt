cbf2cfee83e75386e81ee92a34a1cbb8d0e4922a
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2024-11-25 13:09:06 +0100

    setter group id for å lese fra kafka

--- src/main/kotlin/no/nav/helse/flex/forelagteopplysningerainntekt/ForelagteOpplysningerListener.kt
+++ src/main/kotlin/no/nav/helse/flex/forelagteopplysningerainntekt/ForelagteOpplysningerListener.kt
@@ -17,7 +17,7 @@ class ForelagteOpplysningerListener(
     @KafkaListener(
         topics = [FORELAGTE_OPPLYSNINGER_TOPIC],
         containerFactory = "aivenKafkaListenerContainerFactory",
-        groupId = "forelagteopplysninger-1",
+        groupId = "forelagteopplysninger-2",
     )
     fun listen(
         cr: ConsumerRecord<String, String>,
