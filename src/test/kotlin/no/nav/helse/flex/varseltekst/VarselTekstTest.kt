b4c8a04a14a347c3a758b109faeb5bce06ed9502
Eirik Skjærseth <eirik.skjaerseth@gmail.com>
2025-01-17 14:54:06 +0100

    omdøp VenterPaaInntektsmelding -> VarselTekst

--- src/test/kotlin/no/nav/helse/flex/varseltekst/VarselTekstTest.kt
+++ src/test/kotlin/no/nav/helse/flex/varseltekst/VarselTekstTest.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.EnumSource
 import java.time.LocalDate
 
-class VenterPaaInntektsmeldingTest {
+class VarselTekstTest {
 
     enum class FormaterParams(val tid: LocalDate, val formatert: String) {
         ETT_DAG_SIFFER(LocalDate.parse("2020-01-01"), "1. januar 2020"),
