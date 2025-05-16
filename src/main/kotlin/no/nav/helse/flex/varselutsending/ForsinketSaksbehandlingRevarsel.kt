909c170a91e377211a46d35992f850be81376992
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2025-05-16 10:37:14 +0200

    fikser tekst på logmelding om revarsel

--- src/main/kotlin/no/nav/helse/flex/varselutsending/ForsinketSaksbehandlingRevarsel.kt
+++ src/main/kotlin/no/nav/helse/flex/varselutsending/ForsinketSaksbehandlingRevarsel.kt
@@ -44,7 +44,7 @@ class ForsinketSaksbehandlingRevarselFinnPersoner(
                 .finnPersonerForRevarslingSomVenterPåSaksbehandler(varsletFoer = varsletFør)
 
         val returMap = mutableMapOf<CronJobStatus, Int>()
-        log.info("Fant ${fnrListe.size} unike fnr for varselutsending for forsinket saksbehandling grunnet manglende inntektsmelding")
+        log.info("Fant ${fnrListe.size} unike fnr-kandidater som vil vurderes for varselutsending av revarsel for forsinket saksbehandling")
 
         returMap[CronJobStatus.UNIKE_FNR_KANDIDATER_REVARSEL_FORSINKET_SAKSBEHANDLING] = fnrListe.size
 
