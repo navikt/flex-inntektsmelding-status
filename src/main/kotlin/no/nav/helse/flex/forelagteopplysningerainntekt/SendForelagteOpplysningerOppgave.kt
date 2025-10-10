42e5ee2f8563500a51a46e27ec8ff38a5757e32e
eirikhs <eirik.skjaerseth@gmail.com>
2025-10-10 10:13:15 +0200

    fjern HarForelagtSammeVedtaksperiodeSjekk (#584)

--- src/main/kotlin/no/nav/helse/flex/forelagteopplysningerainntekt/SendForelagteOpplysningerOppgave.kt
+++ src/main/kotlin/no/nav/helse/flex/forelagteopplysningerainntekt/SendForelagteOpplysningerOppgave.kt
@@ -3,7 +3,6 @@ package no.nav.helse.flex.forelagteopplysningerainntekt
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.module.kotlin.readValue
 import no.nav.helse.flex.forelagteopplysningerainntekt.sjekker.ForsinkelseFraOpprinnelseTilVarselSjekk
-import no.nav.helse.flex.forelagteopplysningerainntekt.sjekker.HarForelagtSammeVedtaksperiodeSjekk
 import no.nav.helse.flex.logger
 import no.nav.helse.flex.objectMapper
 import no.nav.helse.flex.toJsonNode
@@ -19,7 +18,6 @@ class SendForelagteOpplysningerOppgave(
     private val forelagteOpplysningerRepository: ForelagteOpplysningerRepository,
     private val hentRelevantInfoTilForelagtOpplysning: HentRelevantInfoTilForelagtOpplysning,
     private val opprettBrukervarselForForelagteOpplysninger: OpprettBrukervarselForForelagteOpplysninger,
-    private val harForelagtSammeVedtaksperiode: HarForelagtSammeVedtaksperiodeSjekk,
     private val forsinkelseFraOpprinnelseTilVarselSjekk: ForsinkelseFraOpprinnelseTilVarselSjekk,
 ) {
     private val log = logger()
@@ -55,22 +53,8 @@ class SendForelagteOpplysningerOppgave(
             return false
         }
 
-        if (harForelagtSammeVedtaksperiode.sjekk(
-                fnr = relevantInfoTilForelagteOpplysninger.fnr,
-                vedtaksperiodeId = forelagteOpplysninger.vedtaksperiodeId,
-                forelagteOpplysningerId = forelagteOpplysninger.id!!,
-            )
-        ) {
-            log.error(
-                "Har forelagt samme vedtaksperiode id tidligere. " +
-                    "Forelagte opplysninger: ${forelagteOpplysninger.id}. " +
-                    "Skal vi forelegge for denne personen på nytt? Mulig vi får feil data fra spleis.",
-            )
-            return false
-        }
-
         opprettBrukervarselForForelagteOpplysninger.opprettVarslinger(
-            varselId = forelagteOpplysninger.id,
+            varselId = forelagteOpplysninger.id!!,
             melding = forelagteOpplysninger.forelagteOpplysningerMelding,
             fnr = relevantInfoTilForelagteOpplysninger.fnr,
             orgNavn = relevantInfoTilForelagteOpplysninger.orgNavn,
