61f705d516f3efd570b3b6f7ef5ed0c0eb0f5a85
Steinar Sørli Hegna <steinar.sorli.hegna@nav.no>
2026-09-08 11:51:20 +0200

    Reprosesser version 2.1 meldinger fra tbd.sis
    Co-authored-by: Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
    Co-authored-by: Lotus <lotus.sophie.vea.simenstad@nav.no>
    Co-authored-by: Malin Haugsdal <malin.haugsdal@nav.no>

---
+++ src/main/kotlin/no/nav/helse/flex/vedtaksperiodebehandling/VedtaksperiodeBehandlingReprosesseringListener.kt
@@ -0,0 +1,56 @@
+package no.nav.helse.flex.vedtaksperiodebehandling
+
+import com.fasterxml.jackson.module.kotlin.readValue
+import no.nav.helse.flex.kafka.SIS_TOPIC
+import no.nav.helse.flex.logger
+import no.nav.helse.flex.objectMapper
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.common.TopicPartition
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.listener.ConsumerSeekAware
+import org.springframework.kafka.listener.ConsumerSeekAware.ConsumerSeekCallback
+import org.springframework.kafka.support.Acknowledgment
+import org.springframework.stereotype.Component
+import java.time.Instant
+import java.time.LocalDate
+import java.time.ZoneOffset
+
+@Component
+class VedtaksperiodeBehandlingReprosesseringListener(
+    private val prosseserKafkaMeldingFraSpleiselaget: ProsseserKafkaMeldingFraSpleiselaget,
+) : ConsumerSeekAware {
+    val log = logger()
+
+    @KafkaListener(
+        topics = [SIS_TOPIC],
+        containerFactory = "aivenKafkaListenerContainerFactory",
+        id = "flex-inntektsmelding-status-vedtaksperiode-behandling-v2-1",
+        idIsGroup = true,
+    )
+    fun listen(
+        cr: ConsumerRecord<String, String>,
+        acknowledgment: Acknowledgment,
+    ) {
+        val meldingMetadata: MeldingMetadata = objectMapper.readValue(cr.value())
+
+        if (meldingMetadata.versjon?.startsWith("2.1") == true && meldingMetadata.eventName == "behandlingstatus") {
+            val kafkaDto: Behandlingstatusmelding = objectMapper.readValue(cr.value())
+            prosseserKafkaMeldingFraSpleiselaget.prosesserKafkaMelding(kafkaDto)
+        }
+
+        acknowledgment.acknowledge()
+    }
+
+    override fun onPartitionsAssigned(
+        assignments: Map<TopicPartition?, Long?>,
+        callback: ConsumerSeekCallback,
+    ) {
+        val startAt = LocalDate.of(2026, 8, 17).toInstantAtStartOfDay().toEpochMilli()
+
+        assignments.keys.filterNotNull().forEach { topicPartition ->
+            callback.seekToTimestamp(topicPartition.topic(), topicPartition.partition(), startAt)
+        }
+    }
+
+    private fun LocalDate.toInstantAtStartOfDay(): Instant = this.atStartOfDay().toInstant(ZoneOffset.UTC)
+}
