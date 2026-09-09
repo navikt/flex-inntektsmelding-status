720768327e231af808c6332568cf0a6355683cdd
Steinar Sørli Hegna <57735098+steinshe@users.noreply.github.com>
2026-09-09 11:35:40 +0200

    Revert "Hold offset up to date imens reprosesseringen kjører (#710)"
    This reverts commit 3110a4fe589108afabe8566a7a5fe9276d169a4a.

--- src/main/kotlin/no/nav/helse/flex/vedtaksperiodebehandling/VedtaksperiodeBehandlingListener.kt
+++ src/main/kotlin/no/nav/helse/flex/vedtaksperiodebehandling/VedtaksperiodeBehandlingListener.kt
@@ -1,27 +1,28 @@
 package no.nav.helse.flex.vedtaksperiodebehandling
 
-import no.nav.helse.flex.kafka.SIS_TOPIC
+import com.fasterxml.jackson.module.kotlin.readValue
 import no.nav.helse.flex.logger
+import no.nav.helse.flex.objectMapper
 import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
 
 @Component
-class VedtaksperiodeBehandlingListener {
+class VedtaksperiodeBehandlingListener(
+    private val prosseserKafkaMeldingFraSpleiselaget: ProsseserKafkaMeldingFraSpleiselaget,
+) {
     val log = logger()
 
-    @KafkaListener(
-        topics = [SIS_TOPIC],
-        containerFactory = "aivenKafkaListenerContainerFactory",
-        id = "flex-inntektsmelding-status-vedtaksperiode-behandling",
-        idIsGroup = false,
-    )
     fun listen(
         cr: ConsumerRecord<String, String>,
         acknowledgment: Acknowledgment,
     ) {
-        log.info("Holder offset up to date imens reprosessering kjører")
+        val meldingMetadata: MeldingMetadata = objectMapper.readValue(cr.value())
+
+        if (meldingMetadata.eventName == "behandlingstatus") {
+            val kafkaDto: Behandlingstatusmelding = objectMapper.readValue(cr.value())
+            prosseserKafkaMeldingFraSpleiselaget.prosesserKafkaMelding(kafkaDto)
+        }
 
         acknowledgment.acknowledge()
     }
