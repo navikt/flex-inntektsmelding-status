111227950593a7f9fd57034f3cf38429dfc13bab
Steinar Sørli Hegna <57735098+steinshe@users.noreply.github.com>
2026-09-09 16:14:44 +0200

    Reduser grenseverdiene igjen etter vi nå har catchet opp (#715)

--- src/main/kotlin/no/nav/helse/flex/varselutsending/ManglendeInntektsmeldingAndreVarsel.kt
+++ src/main/kotlin/no/nav/helse/flex/varselutsending/ManglendeInntektsmeldingAndreVarsel.kt
@@ -34,8 +34,8 @@ class ManglendeInntektsmeldingAndreVarselFinnPersoner(
     environmentToggles: EnvironmentToggles,
 ) {
     private val log = logger()
-    private val maxAntallUtsendelsePerKjoring = if (environmentToggles.isNais()) 2500 else 4
-    private val funksjonellGrenseForAntallVarsler = if (environmentToggles.isNais()) 40000 else 7
+    private val maxAntallUtsendelsePerKjoring = if (environmentToggles.isNais()) 250 else 4
+    private val funksjonellGrenseForAntallVarsler = if (environmentToggles.isNais()) 4000 else 7
 
     fun hentOgProsseser(now: Instant): Map<CronJobStatus, Int> {
         val sendtFoer = now.minus(28, DAYS)
--- src/main/kotlin/no/nav/helse/flex/varselutsending/ManglendeInntektsmeldingFørsteVarsel.kt
+++ src/main/kotlin/no/nav/helse/flex/varselutsending/ManglendeInntektsmeldingFørsteVarsel.kt
@@ -34,8 +34,8 @@ class ManglendeInntektsmeldingFørsteVarselFinnPersoner(
 ) {
     private val log = logger()
 
-    private val maxAntallUtsendelsePerKjoring = if (environmentToggles.isNais()) 2500 else 4
-    private val funksjonellGrenseForAntallVarsler = if (environmentToggles.isNais()) 60000 else 7
+    private val maxAntallUtsendelsePerKjoring = if (environmentToggles.isNais()) 250 else 4
+    private val funksjonellGrenseForAntallVarsler = if (environmentToggles.isNais()) 6000 else 7
 
     fun hentOgProsseser(now: Instant): Map<CronJobStatus, Int> {
         val sendtFoer = now.minus(15, DAYS)
